ea467115fe701f545ff0d7390f48c79d3706ef89
Ben Armston <ben.armston@alces-software.com>
2020-07-17 13:31:20 +0100

    Bump version

--- lib/docs/version.rb
+++ lib/docs/version.rb
@@ -25,6 +25,6 @@
 # https://github.com/alces-flight/flight-docs
 #==============================================================================
 module Docs
-  VERSION = '0.1.5'
+  VERSION = '0.1.7'
   TITLE = 'Alces Flight Center document viewer'
 end
